2714d488bbaa36eae212163c8a295465e53731e6
Rashil <rashil>
2022-09-03 22:53:36 +0530

    more pythonic quick sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,10 @@
 elements = [9, 6, 5, 0, 8, 2, 7, 1, 3, 4]
-sorted_array = []
+
 for index in range(len(elements)):
     if (index != 0) and (elements[index] < elements[index-1]):
         for i in range(len(elements[:index])):
             if elements[index] < elements[i]:
-                tmp = elements[index]
-                del(elements[index])
-                elements.insert(i, tmp)
+                elements.insert(i, elements.pop(index))
             else:
                 continue
     elif index != 0:
